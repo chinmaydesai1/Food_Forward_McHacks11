602b6e38060df08f5e4a53810b4bae968a299774
xdavidg <157243142+xdavidg@users.noreply.github.com>
2024-01-27 23:50:48 -0500

    Update main.py

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -1,13 +1,14 @@
 from flask import Flask
 from flask_cors import CORS
 import db
-from Student import studentBlueprint
+from student import studentBlueprint
 from apscheduler.schedulers.background import BackgroundScheduler
 import atexit
 
 app = Flask(__name__)
 CORS(app)
 db.initialize_db()
+app.register_blueprint(studentBlueprint, url_prefix='/student')
 
 scheduler = BackgroundScheduler()
 
@@ -18,7 +19,6 @@ scheduler.add_job(func=reset_meal_and_snack, trigger="cron", hour=0, minute=0) #
 scheduler.start() #start scheduler
 atexit.register(lambda: scheduler.shutdown()) #shut down when exiting app
 
-app.register_blueprint(studentBlueprint, url_prefix='/student')
 
 if __name__ == "__main__":
     app.run(debug=True)
